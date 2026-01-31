df89cbb05972f1982df43ea029622808edff06d0
ravulapallipadma60-rgb <ravulapalipadma60@gmail.com>
2026-01-31 12:59:10 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from './React';
 
 function App() {
   return (
     <>
     This is first deploy in vercel
+    
     </>
   );
 }
